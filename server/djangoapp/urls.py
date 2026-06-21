cda2195bb329e1eae9057a7b99729bc7a71c6905
oppasskybest-ai <oppasskybest@gmail.com>
2026-06-21 12:06:49 -0400

    Add CarMake CarModel models, populate data, get_cars endpoint, sentiment microservice

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,17 +5,8 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # path for registration
     path(route='register', view=views.registration, name='register'),
-
-    # path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,50 +1,35 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .models import CarMake, CarModel
+from .populate import initiate
 
-
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
+
 def logout_request(request):
-    logout(request)  # Terminate user session
-    data = {"userName": ""}  # Return empty username
+    logout(request)
+    data = {"userName": ""}
     return JsonResponse(data)
 
-# Create a `registration` view to handle sign up request
+
 @csrf_exempt
 def registration(request):
     data = json.loads(request.body)
@@ -56,7 +41,6 @@ def registration(request):
 
     username_exist = False
     try:
-        # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except User.DoesNotExist:
@@ -77,19 +61,24 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+def get_cars(request):
+    if CarMake.objects.count() == 0:
+        initiate()
+    car_makes = CarMake.objects.all()
+    car_models = CarModel.objects.all()
+    make_list = []
+    for make in car_makes:
+        models_list = []
+        for model in car_models.filter(car_make=make):
+            models_list.append({
+                "name": model.name,
+                "type": model.type,
+                "year": model.year
+            })
+        make_list.append({
+            "name": make.name,
+            "description": make.description,
+            "models": models_list
+        })
+    return JsonResponse({"CarMakes": make_list})
